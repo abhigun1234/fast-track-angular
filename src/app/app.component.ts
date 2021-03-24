ba87854eac7b2c81ce353415899f9152fef99abb
Abhishek <abhi.ghosh27feb@gmail.com>
2021-03-24 20:42:54 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mypro';
+   change="hello"
+   showDemo(){
+     console.log("hello")
+     this.change="bye"
+   }
 }
